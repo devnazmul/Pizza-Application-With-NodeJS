2f5c7f364ecdc6f993d5dd51a385a88014a6b8ef
devnazmul <md.nazmul.islam1332@egmail.com>
2021-01-27 09:11:24 +0600

    Aproximately Completed The Designing Part

--- server.js
+++ server.js
@@ -14,10 +14,37 @@ app.set('views', path.join(__dirname, '/resources/views'))
 app.set('view engine', 'ejs')
 //ASSETS
 app.use(express.static('public'))
+
+
+
 //REQ RES
 app.get('/' , (req, res)=>{
     res.render('home')
-})
+});
+app.get('/about' , (req, res)=>{
+    res.render('about')
+});
+app.get('/contact' , (req, res)=>{
+    res.render('contact')
+});
+app.get('/cart' , (req, res)=>{
+    res.render('customer/cart')
+});
+app.get('/registration' , (req, res)=>{
+    res.render('auth/registration')
+});
+app.get('/login' , (req, res)=>{
+    res.render('auth/login')
+});
+app.get('/forgotPass' , (req, res)=>{
+    res.render('auth//forgotPass')
+});
+app.post('/forgotPass' , (req, res)=>{
+    res.render('auth//forgotPass')
+});
+app.get('/menu' , (req, res)=>{
+    res.render('customer/menu')
+});
 
 
 //SERVER
